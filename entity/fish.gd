621edb7addc2ce6745208dd9923a728ec6220825
Olive Perry <wickbox@gmail.com>
2024-10-29 20:47:27 -0700

    flocking and reset on enter

--- entity/fish.gd
+++ entity/fish.gd
@@ -1,31 +1,68 @@
 extends Node2D
 class_name Fish
 
-const sight_radius:float = 30
+const PERCEPTION_RADIUS:float = 60
+const SEPARATION_DISTANCE:float = 40
+const SPEED_MAX:float = 1.0
+
+const ALIGNMENT_FORCE:float = 1
+const COHESION_FORCE:float = 2
+const SEPARATION_FORCE:float = 20
+const FORCE_SCALAR:float = .00005
+
+const MOUSE_FORCE:float = 10
 
 var direction:float = randf_range(0, TAU)
-var speed:float = 1.0
+@onready var velocity:Vector2 = Util.vector_from_angle(SPEED_MAX, direction)
 
 func _process(delta: float) -> void:
-	position += Util.vector_from_angle(speed, direction)
-
 	Util.wrap_screen(self)
 
+	var average_velocity := Vector2.ZERO
+	var average_position := Vector2.ZERO
+	var average_separation := Vector2.ZERO
+	var num_neighbors:int = 0
 
-
-
-
-func coherence() -> Vector2:
-	var close_fish_count:int = 0
-	var average_nearby_pos := Vector2.ZERO
 	var all_fish:Array = get_tree().get_nodes_in_group("fish")
-	for fish:Fish in all_fish:
-		if fish != self:
-			if position.distance_to(fish.position) < sight_radius:
-				close_fish_count += 1
-				average_nearby_pos += fish.position
+	for other:Fish in all_fish:
+		if other == self: continue
 
-	if average_nearby_pos != Vector2.ZERO:
-		average_nearby_pos /= all_fish.size()
+		var distance := position.distance_to(other.position)
 
-	return average_nearby_pos
+		if distance < PERCEPTION_RADIUS:
+			average_velocity += other.velocity
+			average_position += other.position
+
+			if distance < SEPARATION_DISTANCE:
+				var diff:Vector2 = position - other.position
+				diff = diff.limit_length(1.0 / distance)
+				average_separation += diff
+
+			num_neighbors += 1
+
+	if num_neighbors > 0:
+		average_velocity /= num_neighbors
+		average_position /= num_neighbors
+		average_separation /= num_neighbors
+
+	velocity += average_velocity * (ALIGNMENT_FORCE * FORCE_SCALAR)
+	velocity += average_position * (COHESION_FORCE * FORCE_SCALAR)
+	velocity -= average_separation * (SEPARATION_FORCE * FORCE_SCALAR)
+
+	#print(' avg vel %3f   pos %3f   sep  %3f' % [average_velocity.length(), average_position.length(), average_separation.length()])
+
+
+	# and always know about the mouse
+	#var mouse_seaverage_position += Main.mouse_position
+	#if mouse_distance < SEPARATION_DISTANCE:
+		#var diff:Vector2 = position - Main.mouse_position
+		#diff = diff.limit_length(1 / mouse_distance)
+		#average_separation += diff
+	#var mouse_distance := position.distance_to(Main.mouse_position)
+	#var mouse_diff := Main.mouse_position - position
+	#mouse_diff = mouse_diff.limit_length(PERCEPTION_RADIUS)
+	#velocity += mouse_diff * (MOUSE_FORCE * FORCE_SCALAR)
+	#velocity -= average_separation * (SEPARATION_FORCE * FORCE_SCALAR)
+
+	velocity = velocity.limit_length(SPEED_MAX)
+	position += velocity
---
+++ main.gd
@@ -0,0 +1,25 @@
+extends Node2D
+class_name Main
+
+const FISH_SCENE:PackedScene = preload("res://entity/fish.tscn")
+const STARTING_FISH:int = 10
+
+static var mouse_position:Vector2
+
+func _ready() -> void:
+	var screen_size := get_viewport().get_visible_rect().size
+	for i in STARTING_FISH:
+		var fish:Fish = FISH_SCENE.instantiate()
+		fish.position = Vector2(randi_range(0, screen_size.x), randi_range(0, screen_size.y))
+		add_child(fish)
+
+func _process(delta: float) -> void:
+	mouse_position = get_viewport().get_mouse_position()
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept"):
+		var screen_size := get_viewport().get_visible_rect().size
+		var all_fish:Array = get_tree().get_nodes_in_group("fish")
+		for fish:Fish in all_fish:
+			fish.position = Vector2(randi_range(0, screen_size.x), randi_range(0, screen_size.y))
+			fish.velocity = Util.vector_from_angle(Fish.SPEED_MAX, randf_range(0, TAU))
