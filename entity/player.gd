bafcc46b04b5b179f8a4ea0c00be767c55167acb
Clarity Flowers <hello@clarity.flowers>
2024-11-26 22:13:54 -0800

    more anims

--- entity/player.gd
+++ entity/player.gd
@@ -5,20 +5,21 @@ class_name Player
 func _ready():
 	pass # Replace with function body.
 
-const MAX_RUN := 90.0 * 2.0
-const MAX_FALL := 160.0 * 2.0
-const FAST_MAX_ACCEL := 300.0 * 2.0
-const RUN_REDUCE := 400.0 * 2.0
-const RUN_ACCEL := 1000.0 * 2.0
-const GRAVITY := 900.0 * 2.0
-const JUMP_H_BOOST := 40.0 * 2.0
-const JUMP_SPEED := -105.0 * 2.0
-const HALF_GRAV_THRESHOLD := 40.0 * 2.0
+const MAX_RUN := 90.0 * 1.5
+const MAX_FALL := 160.0 * 1.5
+const FAST_MAX_ACCEL := 300.0 * 1.5
+const RUN_REDUCE := 400.0 * 1.5
+const RUN_ACCEL := 1000.0 * 1.5
+const GRAVITY := 900.0 * 1.5
+const JUMP_H_BOOST := 40.0 * 1.5
+const JUMP_SPEED := -105.0 * 1.5
+const HALF_GRAV_THRESHOLD := 40.0 * 1.5
 const VAR_JUMP_TIME = 0.2
 
 var lift_speed := Vector2(0, 0)
 var max_fall := 0.0
 var var_jump_timer := 0.0
+var var_jump_speed := 0.0
 
 var lift_boost: Vector2:
 	get:
@@ -50,20 +51,18 @@ func _process(delta):
 
 	if var_jump_timer > 0:
 		var_jump_timer -= delta
-		
-
-	match state:
-		State.Idle:
-			if move_x != 0:
-				state = State.Walking
-				$AnimationPlayer.stop()
-				$AnimationPlayer.play("walk")
-
-		State.Walking:
-			if move_x == 0:
-				state = State.Idle
-				$AnimationPlayer.stop()
-				$AnimationPlayer.play("idle")
+	
+		if var_jump_timer <= 0:
+			var_jump_timer = 0
+			$AnimationPlayer.stop()
+			$AnimationPlayer.play("fall")
+	
+	
+	if is_on_floor():
+		if move_x != 0:
+			$AnimationPlayer.play("walk")
+		else:
+			$AnimationPlayer.play("idle")
 
 	# horizontal movement
 	if absf(velocity.x) > MAX_RUN && signf(velocity.x) == move_x:
@@ -82,18 +81,31 @@ func _process(delta):
 		var max = max_fall
 		var mult = 0.5 if (absf(velocity.y) < HALF_GRAV_THRESHOLD && (Input.is_action_pressed("Jump"))) else 1.0
 		velocity.y = _approach(velocity.y, max, GRAVITY * mult * delta)
+		if velocity.y > 0:
+			$AnimationPlayer.play("fall")
 		
 		
 	if is_on_floor() and Input.is_action_just_pressed("Jump"):
 		print("Jump! %s" % [max_fall])
+		$AnimationPlayer.stop()
+		$AnimationPlayer.play("jump")
 		var_jump_timer = VAR_JUMP_TIME
 		velocity.x += JUMP_H_BOOST * move_x
 		velocity.y = JUMP_SPEED
 		velocity += lift_boost
-		$AnimatedSprite2D.scale = Vector2(0.6 * signf($AnimatedSprite2D.scale.x), 1.4)
+		var_jump_speed = velocity.y
+		$AnimatedSprite2D.scale = Vector2(0.8 * signf($AnimatedSprite2D.scale.x), 1.2)
+		
+	if var_jump_timer > 0:
+		if Input.is_action_pressed("Jump"):
+			velocity.y = minf(velocity.y, var_jump_speed)
+		else:
+			var_jump_timer = 0
+			#$AnimationPlayer.stop()
+			#$AnimationPlayer.play("fall")
 		
 	move_and_slide()
-	
+		
 	
 	for collision_i in range(get_slide_collision_count()):
 		var collision = get_slide_collision(collision_i)
