85f35f884bd871aaac6f1d5dac7c2270e0941958
Clarity Flowers <hello@clarity.flowers>
2024-11-26 19:42:47 -0800

    celeste's running code

--- entity/player.gd
+++ entity/player.gd
@@ -5,10 +5,30 @@ extends Node2D
 func _ready():
 	pass # Replace with function body.
 
+const MAX_RUN := 90.0
+const RUN_REDUCE := 400.0
+const RUN_ACCEL := 1000.0
+
+
+var move_x := 0.0
+var speed: Vector2 = Vector2(0, 0)
+
+func _approach(value: float, target: float, amount: float) -> float:
+	if value < target:
+		return minf(value + amount, target)
+	if value > target:
+		return maxf(value - amount, target)
+	else:
+		return target
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed("Move left"):
-		position.x -= 5
-	elif Input.is_action_pressed("Move right"):
-		position.x += 5
+	move_x = Input.get_axis("Move left", "Move right")
+	
+	if absf(speed.x) > MAX_RUN && signf(speed.x) == move_x:
+		speed.x = _approach(speed.x, MAX_RUN * move_x, RUN_REDUCE * delta)
+	else:
+		speed.x = _approach(speed.x, MAX_RUN * move_x, RUN_ACCEL * delta)
+		
+	position += speed * delta
