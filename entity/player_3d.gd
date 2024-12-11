99c71e013ba5ab2c38383ca348236c66bad05235
Clarity Flowers <hello@clarity.flowers>
2024-12-10 21:04:35 -0800

    collisions

--- entity/player_3d.gd
+++ entity/player_3d.gd
@@ -12,8 +12,8 @@ func _physics_process(delta):
 	$"Rogue/Rig/Skeleton3D/Knife".hide()
 	$"Rogue/Rig/Skeleton3D/Knife_Offhand".hide()
 	# Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
+	if not is_on_floor():
+		velocity += get_gravity() * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
