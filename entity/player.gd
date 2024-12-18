1d250504cdb7da2b84f3929c8ba0ae53f46b962c
Clarity Flowers <hello@clarity.flowers>
2024-12-17 18:59:12 -0800

    bouncing

--- entity/player.gd
+++ entity/player.gd
@@ -53,6 +53,10 @@ func _enter_tree():
 	max_fall = MAX_FALL
 
 
+func bounce():
+	print("bounce!!!")
+	velocity.y = -velocity.y
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_key_pressed(KEY_R):
